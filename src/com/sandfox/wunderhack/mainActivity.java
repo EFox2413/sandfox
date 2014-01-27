412a600872487801a93d9367cb21fc839344cd9d
elliot fox <elliot@lapFox>
2014-01-27 04:27:59 -0500

    Got file IO working, markers are now persistent

--- src/com/sandfox/wunderhack/mainActivity.java
+++ src/com/sandfox/wunderhack/mainActivity.java
@@ -1,22 +1,29 @@
 package com.sandfox.wunderhack;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.model.*;
 
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -27,58 +34,40 @@ public class mainActivity extends FragmentActivity
 
 	private GoogleMap mMap;
 	private int addMarkCheck = 0;
-	private double lat, lon;
 	private String FILENAME = "coords";
-	private byte [] fisbyte;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         initMap();
+        readFromInternal();
         
-        try {
-			FileInputStream fis = openFileInput(FILENAME);
-		//	String coords = String(fis.read());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
         mMap.setOnMapClickListener(new OnMapClickListener() {
     	
         	@Override
     		public void onMapClick(LatLng arg0) {
     			if (addMarkCheck == 1){
     				mMap.addMarker(new MarkerOptions()
-    						.position(arg0));
+    					.position(arg0));
     				
-    				lat = arg0.latitude;
-    				lon = arg0.longitude;
+    				double lat = arg0.latitude;
+    				double lon = arg0.longitude;
+    				String coord = arg0.latitude + "," + arg0.longitude + "\n";
     				
-    				Toast.makeText(getApplicationContext(), arg0.latitude + "" 
-    								+ arg0.longitude, 1).show();
-    				    				
-    				    				
+    				Toast.makeText(getApplicationContext(), coord, 1).show();
+    				
+    				try {
+						writeToInternal(coord);
+					} catch (IOException e) {
+						Toast.makeText(getApplicationContext(), "Failed to write", 1).show();
+						e.printStackTrace();
+					}
     				addMarkCheck = 0;
     			}
     		}
     	});
-    }
-    
-    public void writeToInternal() throws IOException {
-        FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-        fos.write((lat+""+lon).getBytes());
-        fos.close();
-    }
-    
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    }//end of onCreate()
     
     @Override
     protected void onResume() {
@@ -86,19 +75,75 @@ public class mainActivity extends FragmentActivity
         initMap();
     }
     
+    public void writeToInternal(String str) throws IOException {       
+    	FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND); 
+    	
+        fos.write(str.getBytes());
+        fos.close();
+    }
+    
+    public void readFromInternal() {
+    	try {
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(openFileInput(FILENAME)));
+			 
+					String str;
+					StringBuffer strBuf = new StringBuffer();
+								 
+					while ((str = in.readLine()) != null) {
+						
+						String[] bits = str.toString().split(",",2);
+						bits[1] = bits[1].substring (0, bits[1].length());
+						
+						Toast.makeText(getApplicationContext(), bits[0] + " , " + bits[1], 1).show();
+						
+					    double lat = Double.parseDouble(bits[0]);
+					    double lon = Double.parseDouble(bits[1]);
+					    LatLng coorda = new LatLng(lat, lon);
+					    
+					    Toast.makeText(getApplicationContext(), coorda.latitude + "" 
+								+ coorda.longitude, 1).show();
+					    
+						mMap.addMarker(new MarkerOptions()
+					    	.position(coorda));
+					}
+			        in.close();
+		} 
+        catch (FileNotFoundException e) 
+        {
+			e.printStackTrace();
+			Toast.makeText(getApplicationContext(), "FNF", Toast.LENGTH_SHORT).show();
+		} 
+        catch (UnsupportedEncodingException e) 
+	    {
+			Toast.makeText(getApplicationContext(), e.getMessage(), 1).show();
+	    } 
+	    catch (IOException e) 
+	    {
+	    	Toast.makeText(getApplicationContext(), e.getMessage(), 1).show();
+	    }
+	    catch (Exception e)
+	    {
+	    	Toast.makeText(getApplicationContext(), e.getMessage(), 1).show();
+	    }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    
     private void initMap() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
         }
-        
-        
     }
     
     @Override
@@ -114,15 +159,10 @@ public class mainActivity extends FragmentActivity
                 return super.onOptionsItemSelected(item);
         }
     }
-    
-    private void setUpMap() {
-        
-    }
 
 	@Override
 	public void onMapClick(LatLng arg0) {
-		// TODO Auto-generated method stub
-		
+	
 	}
-    
+	
 }
--- src/com/sandfox/wunderhack/optionsActivity.java
+++ src/com/sandfox/wunderhack/optionsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.Html;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class optionsActivity extends Activity {
@@ -13,6 +14,13 @@ public class optionsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.options_activity);
-
+    }
+    
+    public void onRadioButtonClicked(View v) {
+    	
+    }
+    
+    public void saveOptions(View view) {
+    	finish();
     }
 }
